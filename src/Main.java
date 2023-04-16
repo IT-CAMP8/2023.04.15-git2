49d4b95eb4c12a807a1af571710f2d808a983bc1
Mateusz <mat.bereda@gmail.com>
2023-04-16 20:36:09 +0200

    update

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String temp = scanner.nextLine();
+        int x = Integer.parseInt(temp);
+
+        String y = scanner.nextLine();
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
